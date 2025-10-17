abc0d1c281684f9b9f863d0a1ff6251f25d9a99a
Caden Lee <maltyg3332@gmail.com>
2025-10-17 12:37:33 -0700

    feat: basic page layout system

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import type { ReactNode } from 'react';
+
+function TopBar() {
+  return (
+    <div className="top-bar">
+      TOP BAR
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="top-bar">
+      FOOTER
+    </div>
+  );
+}
+
+function PageFrame(props: { children: ReactNode }) {
+  return (
+    <div className="outer-wrapper">
+      <div className="inner-wrapper">
+        <TopBar />
+        {props.children}
+        <Footer />
+      </div>
+    </div>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <PageFrame>
+      <div className="section">
+        <h2>Section</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="section">
+        <h2>Section 2</h2>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </PageFrame>
+  );
 }
 
 export default App
