a5c8d640f57b86a332e8e69a9227d52a4431adf2
Caden Lee <maltyg3332@gmail.com>
2026-06-13 19:20:54 -0700

    refactor: consolidate duplicated components

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,13 +4,12 @@ import './Home.css';
 import { useRef } from 'react';
 
 import PageFrame from '../components/PageFrame';
-import Showcase, { RoadmapTree, ToolsList, PeekingContainer, PeekingContainerVert } from '../components/Showcase';
+import Showcase, { RoadmapTree, ToolsList, PeekingContainerVert } from '../components/Showcase';
+import { GamesGrid, MakingLifeEasierRow } from '../components/SharedShowcases';
 import Marquee from '../components/Marquee';
 import MyTools from '../components/MyTools';
 
 import TypescriptPlain from 'devicons-react/icons/TypescriptPlain';
-import JavascriptPlain from 'devicons-react/icons/JavascriptPlain';
-import Html5Plain from 'devicons-react/icons/Html5Plain';
 import ReactOriginal from 'devicons-react/icons/ReactOriginal';
 import PostgresqlPlain from 'devicons-react/icons/PostgresqlPlain';
 import SassOriginal from 'devicons-react/icons/SassOriginal';
@@ -21,13 +20,6 @@ import LinkedinOriginal from 'devicons-react/icons/LinkedinOriginal';
 import FirebaseOriginal from 'devicons-react/icons/FirebaseOriginal';
 import TensorflowOriginal from 'devicons-react/icons/TensorflowOriginal';
 import PythonPlain from 'devicons-react/icons/PythonPlain';
-import RustOriginal from 'devicons-react/icons/RustOriginal';
-import Css3Plain from 'devicons-react/icons/Css3Plain';
-import TailwindcssOriginal from 'devicons-react/icons/TailwindcssOriginal';
-import VitejsPlain from 'devicons-react/icons/VitejsPlain';
-import SocketioOriginal from 'devicons-react/icons/SocketioOriginal';
-import CplusplusPlain from 'devicons-react/icons/CplusplusPlain';
-import OpenglPlain from 'devicons-react/icons/OpenglPlain';
 
 import { MdArrowOutward, MdArrowForward, MdMoreHoriz, MdPlayArrow, MdMusicNote, MdRestaurant } from 'react-icons/md';
 
@@ -39,11 +31,6 @@ import CodeEditor from '../assets/images/CodeEditor.png';
 import AAPDetail from '../assets/images/projects/AAP_Detail.png';
 import KKGroup from '../assets/images/projects/KK_Group.jpg';
 import KKDemoPhone from '../assets/images/projects/KK_DemoPhone.png';
-import SheatfishDetail from '../assets/images/projects/Sheatfish_Detail.png';
-import RicalTerminalDetail from '../assets/images/projects/RicalTerminal_Detail.png';
-import HomepageOmniDetail from '../assets/images/projects/HomepageOmni_Detail.jpg';
-import Landform from '../assets/images/projects/Landform.png';
-import PlatSciFi from '../assets/images/projects/Platscifi.png';
 
 import { LinkButton } from '../components/Button';
 import { NavLink } from 'react-router-dom';
@@ -179,109 +166,11 @@ function Home() {
       <div className="section">
         <h2>Making Life Easier</h2>
         Here are some open-source tools I've made to improve the experience of using a computer.
-        <div className="showcase-row">
-          <Showcase className="rical" href="https://github.com/Cadecraft/rical/">
-            <h3>Rical</h3>
-            <span>Cross-device calendar app for minimalists</span>
-            <img src={RicalTerminalDetail} />
-            <ToolsList>
-              <RustOriginal />
-            </ToolsList>
-          </Showcase>
-          <Showcase className="sheatfish" href="https://github.com/Cadecraft/sheatfish">
-            <h3>Sheatfish</h3>
-            <span>Lightweight, efficient terminal spreadsheet editor</span>
-            <img src={SheatfishDetail} />
-            <ToolsList>
-              <RustOriginal />
-            </ToolsList>
-          </Showcase>
-          <Showcase className="homepage-omni" href="https://github.com/Cadecraft/HomepageOmni">
-            <h3>HomepageOmni</h3>
-            <span>Configurable homepage for your browser</span>
-            <img src={HomepageOmniDetail} />
-            <ToolsList>
-              <Html5Plain />
-              <JavascriptPlain />
-            </ToolsList>
-          </Showcase>
-        </div>
+        <MakingLifeEasierRow />
       </div>
       <div className="section">
         <h2>Games I've Built</h2>
-        <div className="games-grid">
-          <Showcase className="tetrome" href="https://github.com/Cadecraft/tetrome-source">
-            <div>
-              <div>
-                <h3>Tetrome</h3>
-                <span>A full-featured stacker game. Add the extension to your browser!</span>
-              </div>
-              <img src="https://github.com/Cadecraft/tetrome-source/raw/master/repo_images/TetromeDemo.gif" />
-            </div>
-            <ToolsList>
-              <JavascriptPlain />
-              <Html5Plain />
-              <Css3Plain />
-            </ToolsList>
-          </Showcase>
-          <Showcase className="landform-eclipse" href="https://github.com/Cadecraft/Cadecraft.github.io/tree/main/static/landform-eclipse">
-            <div>
-              <div>
-                <h3>Landform: Eclipse</h3>
-                <span>A 2d open-world exploration web game with procedural generation</span>
-              </div>
-              <PeekingContainer>
-                <img src={Landform} />
-              </PeekingContainer>
-            </div>
-            <ToolsList>
-              <JavascriptPlain />
-              <Html5Plain />
-              <Css3Plain />
-            </ToolsList>
-          </Showcase>
-          <Showcase className="speedstreets" href="https://github.com/Big-Dyl/IrvineHacks2025">
-            <div>
-              <div>
-                <h3>SpeedStreets</h3>
-                <span>This multiplayer geography game tests how well you know the streets in your city, built for IrvineHacks 2025</span>
-              </div>
-              <PeekingContainer>
-                <img src="https://github.com/Big-Dyl/IrvineHacks2025/raw/main/screenshot/Screenshot_Game.png"  />
-              </PeekingContainer>
-            </div>
-            <ToolsList>
-              <TypescriptPlain />
-              <ReactOriginal />
-              <VitejsPlain />
-              <TailwindcssOriginal />
-              <SocketioOriginal />
-            </ToolsList>
-          </Showcase>
-          <Showcase className="regnatural">
-            <h3>Re:Gnatural</h3>
-            <span>Coming soon: a time/physics-based simulation game inspired by falling sand</span>
-            <ToolsList>
-              <CplusplusPlain />
-              <OpenglPlain />
-            </ToolsList>
-          </Showcase>
-          <Showcase className="platscifi" href="https://github.com/tivect/PlatScifi">
-            <div>
-              <div>
-                <h3>PlatSciFi</h3>
-                <span>An experimental programmer's platformer game that lets you define levels by writing files in a custom language</span>
-              </div>
-              <PeekingContainer>
-                <img src={PlatSciFi} />
-              </PeekingContainer>
-            </div>
-            <ToolsList>
-              <CplusplusPlain />
-              <OpenglPlain />
-            </ToolsList>
-          </Showcase>
-        </div>
+        <GamesGrid />
       </div>
       <div className="section">
         <h2 ref={anchorOtherInterests}>Other Interests</h2>
--- frontend/src/pages/ShowcasePage.tsx
+++ frontend/src/pages/ShowcasePage.tsx
@@ -2,29 +2,13 @@ import '../App.css';
 import './ShowcasePage.css';
 
 import PageFrame from '../components/PageFrame';
-import Showcase, { ToolsList, PeekingContainer } from '../components/Showcase';
+import { GamesGrid, MakingLifeEasierRow } from '../components/SharedShowcases';
 
-import TypescriptPlain from 'devicons-react/icons/TypescriptPlain';
-import JavascriptPlain from 'devicons-react/icons/JavascriptPlain';
-import Html5Plain from 'devicons-react/icons/Html5Plain';
-import ReactOriginal from 'devicons-react/icons/ReactOriginal';
 import GithubOriginal from 'devicons-react/icons/GithubOriginal';
-import RustOriginal from 'devicons-react/icons/RustOriginal';
-import Css3Plain from 'devicons-react/icons/Css3Plain';
-import TailwindcssOriginal from 'devicons-react/icons/TailwindcssOriginal';
-import VitejsPlain from 'devicons-react/icons/VitejsPlain';
-import SocketioOriginal from 'devicons-react/icons/SocketioOriginal';
-import CplusplusPlain from 'devicons-react/icons/CplusplusPlain';
-import OpenglPlain from 'devicons-react/icons/OpenglPlain';
 
 import { MdPlayArrow, MdMusicNote } from 'react-icons/md';
 
 import CadecraftStrip from '../assets/images/logo_CadecraftStrip_v2.png';
-import SheatfishDetail from '../assets/images/projects/Sheatfish_Detail.png';
-import RicalTerminalDetail from '../assets/images/projects/RicalTerminal_Detail.png';
-import HomepageOmniDetail from '../assets/images/projects/HomepageOmni_Detail.jpg';
-import Landform from '../assets/images/projects/Landform.png';
-import PlatSciFi from '../assets/images/projects/Platscifi.png';
 
 import { LinkButton } from '../components/Button';
 import { NavLink } from 'react-router-dom';
@@ -91,111 +75,13 @@ function ShowcasePage() {
             <h2>Making Life Easier</h2>
           </div>
           Here are some open-source tools I've made to improve the experience of using a computer.
-          <div className="showcase-row">
-            <Showcase className="rical" href="https://github.com/Cadecraft/rical/">
-              <h3>Rical</h3>
-              <span>Cross-device calendar app for minimalists</span>
-              <img src={RicalTerminalDetail} />
-              <ToolsList>
-                <RustOriginal />
-              </ToolsList>
-            </Showcase>
-            <Showcase className="sheatfish" href="https://github.com/Cadecraft/sheatfish">
-              <h3>Sheatfish</h3>
-              <span>Lightweight, efficient terminal spreadsheet editor</span>
-              <img src={SheatfishDetail} />
-              <ToolsList>
-                <RustOriginal />
-              </ToolsList>
-            </Showcase>
-            <Showcase className="homepage-omni" href="https://github.com/Cadecraft/HomepageOmni">
-              <h3>HomepageOmni</h3>
-              <span>Configurable homepage for your browser</span>
-              <img src={HomepageOmniDetail} />
-              <ToolsList>
-                <Html5Plain />
-                <JavascriptPlain />
-              </ToolsList>
-            </Showcase>
-          </div>
+          <MakingLifeEasierRow />
         </div>
         <div className="section">
           <div className="highlight">
             <h2>Games I've Built</h2>
           </div>
-          <div className="games-grid">
-            <Showcase className="tetrome" href="https://github.com/Cadecraft/tetrome-source">
-              <div>
-                <div>
-                  <h3>Tetrome</h3>
-                  <span>A full-featured stacker game. Add the extension to your browser!</span>
-                </div>
-                <img src="https://github.com/Cadecraft/tetrome-source/raw/master/repo_images/TetromeDemo.gif" />
-              </div>
-              <ToolsList>
-                <JavascriptPlain />
-                <Html5Plain />
-                <Css3Plain />
-              </ToolsList>
-            </Showcase>
-            <Showcase className="landform-eclipse" href="https://github.com/Cadecraft/Cadecraft.github.io/tree/main/static/landform-eclipse">
-              <div>
-                <div>
-                  <h3>Landform: Eclipse</h3>
-                  <span>A 2d open-world exploration web game with procedural generation</span>
-                </div>
-                <PeekingContainer>
-                  <img src={Landform} />
-                </PeekingContainer>
-              </div>
-              <ToolsList>
-                <JavascriptPlain />
-                <Html5Plain />
-                <Css3Plain />
-              </ToolsList>
-            </Showcase>
-            <Showcase className="speedstreets" href="https://github.com/Big-Dyl/IrvineHacks2025">
-              <div>
-                <div>
-                  <h3>SpeedStreets</h3>
-                  <span>This multiplayer geography game tests how well you know the streets in your city, built for IrvineHacks 2025</span>
-                </div>
-                <PeekingContainer>
-                  <img src="https://github.com/Big-Dyl/IrvineHacks2025/raw/main/screenshot/Screenshot_Game.png"  />
-                </PeekingContainer>
-              </div>
-              <ToolsList>
-                <TypescriptPlain />
-                <ReactOriginal />
-                <VitejsPlain />
-                <TailwindcssOriginal />
-                <SocketioOriginal />
-              </ToolsList>
-            </Showcase>
-            <Showcase className="regnatural">
-              <h3>Re:Gnatural</h3>
-              <span>Coming soon: a time/physics-based simulation game inspired by falling sand</span>
-              <ToolsList>
-                <CplusplusPlain />
-                <OpenglPlain />
-              </ToolsList>
-            </Showcase>
-            <Showcase className="platscifi" href="https://github.com/tivect/PlatScifi">
-              <div>
-                <div>
-                  <h3>PlatSciFi</h3>
-                  <span>An experimental programmer's platformer game that lets you define levels by writing files in a custom language</span>
-                </div>
-                <PeekingContainer>
-                  <img src={PlatSciFi} />
-                </PeekingContainer>
-              </div>
-              <ToolsList>
-                <CplusplusPlain />
-                <OpenglPlain />
-              </ToolsList>
-            </Showcase>
-          </div>
+          <GamesGrid />
         </div>
         <div className="section">
           <div className="highlight">
